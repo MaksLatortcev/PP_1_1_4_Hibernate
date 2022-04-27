db211dc5baf901c6b987917f89d4ded5afda37f7
MaksLatortcev <maksim_anatolievish@mail.ru>
2022-04-27 16:03:18 +0300

    all dane

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,33 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+
+        userService.saveUser("Пётр", "Петров", (byte) 22);
+        System.out.println("User с именем – Пётр добавлен в базу данных");
+        userService.saveUser("Иван", "Иванов", (byte) 33);
+        System.out.println("User с именем – Иван добавлен в базу данных");
+        userService.saveUser("Семён", "Семёнов", (byte) 44);
+        System.out.println("User с именем – Семён добавлен в базу данных");
+        userService.saveUser("Григорий", "Григорьев", (byte) 55);
+        System.out.println("User с именем – Григорий добавлен в базу данных");
+
+        List<User> users = userService.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private final String connectionUrl;
+    private final String userName;
+    private final String password;
+
+    public Util() throws ClassNotFoundException {
+        connectionUrl = "jdbc:mysql://localhost:3306/user_113";
+        userName = "kata";
+        password = "123";
+        Class.forName("com.mysql.cj.jdbc.Driver");
+    }
+
+    public Connection getConnection() throws SQLException {
+        Connection connection = DriverManager.getConnection(connectionUrl, userName, password);
+        connection.setAutoCommit(false);
+        return connection;
+    }
 }
